1c98c8a78276a85e10a956a97a8bfdc1b76a43c1
kurtis <kurtisdauz@gmail.com>
2020-03-13 09:05:12 -0400

    Added new astoried, explsion animtation and coding
    WRYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY

---
+++ Assets/Scrpits/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+        [Header("Wave Settings")]
+        public GameObject hazard;
+        public Vector2 spawnValue;
+        public int hazardCount;
+
+        [Header("Wave Time Settings")]
+        public float startWait;
+        public float spawnWait;
+        public float waveWait;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
